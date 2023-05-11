106dcabd24c0ca66cab81a6abf94709e95fe99d9
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-11 11:12:59 -0700

    basic logic, state

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './css/App.css';
 
 function App() {
+  const [rating, setRating] = useState(5);
+  const [submit, setSubmit] = useState(false);
+
+  const ratingOptions = [1, 2, 3, 4, 5];
+
+  if (submit) {
+    console.log(`${rating}`);
+  }
+
   return (
     <div className="App">
       <div>
+
+        {!submit 
+        
+        ?
+
         <div>
           <h2>How did we do?</h2>
           <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
           <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
+            <button onClick={() => setRating(1)}>1</button>
+            <button onClick={() => setRating(2)}>2</button>
+            <button onClick={() => setRating(3)}>3</button>
+            <button onClick={() => setRating(4)}>4</button>
+            <button onClick={() => setRating(5)}>5</button>
           </div>
-          <button>Submit</button>
+          <button type="submit" onClick={() => setSubmit(true)}>Submit</button>
         </div>
 
+        :
+
         <div>
-          <h2>You selected Add rating here out of 5</h2>
+          <h2>You selected {rating} out of 5</h2>
           <p>Thank you!</p>
           <p>We appreciate you taking the time to give a rating. If you ever need more support, don&apos;t hesitate to get in touch!</p>
         </div>
+
+        }
+        
       </div>
 
       <footer className="attribution">
