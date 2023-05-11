387d148d11ef007bc7b973fc00478721fd571ec8
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-10 20:28:44 -0700

    create react app tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
