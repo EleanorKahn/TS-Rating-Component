5536b5daa4d059ff17c234d35fbfaa5415ac29a4
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-12 21:31:15 -0700

    font, tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import("tailwindcss").Config}*/
+
+module.exports = {
+    content: ["./src/**/.{js, jsx, ts, tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "veryDarkBlue": "hsl(216, 12%, 8%)",
+                "darkBlue": "hsl(213, 19%, 18%)",
+                "mediumGrey": "hsl(216, 12%, 54%)",
+                "lightGrey": "hsl(217, 12%, 63%)",
+                "orange": "hsl(25, 97%, 53%)",
+            }
+        }
+    }
+}
