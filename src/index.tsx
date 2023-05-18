6a5da505c22cb40264b2d02e5f6ab6e0251136fc
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-18 11:52:36 -0700

    file cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
-// import "./css/main.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(
