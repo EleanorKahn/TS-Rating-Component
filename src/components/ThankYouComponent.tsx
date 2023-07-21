dadd61691d92a9b4f2c9fc1a536ed50c25b1e2d5
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-21 16:06:29 -0700

    Thank you component color, footer style

--- src/components/ThankYouComponent.tsx
+++ src/components/ThankYouComponent.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 const ThankYouComponent = (props: { rating: number }) => {
   return (
     <div>
-        <h2 className='user-rating'>You selected {props.rating} out of 5</h2>
-        <p className='thank-you'>Thank you!</p>
+      <img src='/images/illustration-thank-you.svg' alt='a robot printing something THINK OF A BETTER ALT'></img>
+        <h3 className='user-rating'>You selected {props.rating} out of 5</h3>
+        <h2 className='thank-you'>Thank you!</h2>
         <p className='ty-paragraph'>We appreciate you taking the time to give a rating. If you ever need more support, don&apos;t hesitate to get in touch!</p>
     </div>
   );
