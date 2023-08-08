13858ce03a95d15408f272dbb68ec7c7922aec8a
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-08-07 23:32:53 -0700

    thank you styling

--- src/components/ThankYouComponent.tsx
+++ src/components/ThankYouComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ThankYouComponent = (props: { rating: number }) => {
   return (
     <div className='thank-you-container'>
-      <img src='/images/illustration-thank-you.svg' alt='a robot printing something THINK OF A BETTER ALT'></img>
+      <img className='robot' src='/images/illustration-thank-you.svg' alt='a robot printing something THINK OF A BETTER ALT'></img>
         <h3 className='user-rating'>You selected {props.rating} out of 5</h3>
         <h2 className='thank-you'>Thank you!</h2>
         <p className='ty-paragraph'>We appreciate you taking the time to give a rating. If you ever need more support, don&apos;t hesitate to get in touch!</p>
